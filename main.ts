feda7c6692cae3f0db62d30097119c4062880455
Rob Wilson <rob@rwilson.org.uk>
2021-01-02 15:01:15 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+// Combine the 3 radio messages into 1 line of text
+radio.onReceivedString(function (receivedString) {
+    // Detect the 1st message
+    if (receivedString.charAt(0) == "*") {
+        count = 0
+        list = []
+        // Strip off the leading * and add the message to list
+        // Strip off the leading * and add the message to list
+        list[0] = receivedString.substr(1, 16)
+    } else {
+        // Add the 2nd and 3rd mesages
+        list.push(receivedString)
+        count += 1
+        // Now send the messages to USB as 1 line of text
+        if (count == 2) {
+            for (let index = 0; index <= 2; index++) {
+                serial.writeString("" + list[index] + ",")
+            }
+            serial.writeLine("")
+        }
+    }
+})
+let list: string[] = []
+let count = 0
+radio.setGroup(1)
